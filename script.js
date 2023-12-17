eb26cb9412cd19e85bd2a94abfdf1699d6b118ac
Simriti <simritib1@gmail.com>
2023-12-17 15:01:40 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,26 @@
 // Get references to the input elements
 const topLeftInput = document.getElementById('topLeft');
+const topLeftValue = document.getElementById('topLeftValue');
+
 const topRightInput = document.getElementById('topRight');
+const topRightValue = document.getElementById('topRightValue');
+
 const bottomLeftInput = document.getElementById('bottomLeft');
+const bottomLeftValue = document.getElementById('bottomLeftValue');
+
 const bottomRightInput = document.getElementById('bottomRight');
+const bottomRightValue = document.getElementById('bottomRightValue');
+
+const box = document.getElementById('previewBox');
+const boxColorInput = document.getElementById('boxColor');
 
 // Add input event listeners to update preview on range input change
 topLeftInput.addEventListener('input', updatePreview);
 topRightInput.addEventListener('input', updatePreview);
 bottomLeftInput.addEventListener('input', updatePreview);
 bottomRightInput.addEventListener('input', updatePreview);
+box.addEventListener('click', changeColor);
+boxColorInput.addEventListener('input', updateBoxColor);
 
 function updatePreview() {
     // Get the input values
@@ -20,6 +32,12 @@ function updatePreview() {
     // Update the border-radius property of the preview box
     const previewBox = document.getElementById('previewBox');
     previewBox.style.borderRadius = `${topLeft} ${topRight} ${bottomLeft} ${bottomRight}`;
+
+    // Update the display values
+    topLeftValue.textContent = topLeft;
+    topRightValue.textContent = topRight;
+    bottomLeftValue.textContent = bottomLeft;
+    bottomRightValue.textContent = bottomRight;
 }
 
 function copyToClipboard() {
@@ -30,4 +48,62 @@ function copyToClipboard() {
     navigator.clipboard.writeText(cssCode)
         .then(() => alert('CSS code copied to clipboard!'))
         .catch(err => console.error('Unable to copy to clipboard', err));
+}
+
+// Change the background color of the box
+function changeColor() {
+    const randomColor = getRandomColor();
+    box.style.backgroundColor = randomColor;
+    boxColorInput.value = randomColor;
+}
+
+// Generate a random hex color
+function getRandomColor() {
+    return '#' + Math.floor(Math.random() * 16777215).toString(16);
+}
+
+// Get the color value from the color picker and update the box color
+function updateBoxColor() {   
+    const boxColor = boxColorInput.value;
+    box.style.backgroundColor = boxColor;
+}
+
+
+
+// Add event listeners for drag and drop
+box.addEventListener('dragstart', handleDragStart);
+box.addEventListener('dragend', handleDragEnd);
+
+let initialX, initialY;
+
+function handleDragStart(e) {
+    e.dataTransfer.setData('text/plain', ''); // required for Firefox
+    initialX = e.clientX - box.getBoundingClientRect().left;
+    initialY = e.clientY - box.getBoundingClientRect().top;
+}
+
+function handleDragEnd() {
+    // Return the box to its original position
+    box.style.transition = 'transform 0.3s ease';
+    box.style.transform = 'translate(0, 0)';
+    setTimeout(() => {
+        box.style.transition = ''; // reset transition after animation
+    }, 300);
+}
+
+// Add event listeners for drag and drop
+document.addEventListener('dragover', handleDragOver);
+document.addEventListener('drop', handleDrop);
+
+function handleDragOver(e) {
+    e.preventDefault();
+}
+
+function handleDrop(e) {
+    e.preventDefault();
+
+    const dropX = e.clientX - initialX;
+    const dropY = e.clientY - initialY;
+
+    box.style.transform = `translate(${dropX}px, ${dropY}px)`;
 }
